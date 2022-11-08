3c5254fc7a17263fec186528e4ba40b70f754d2c
rachel howard <rdfields13@gmail.com>
2022-11-08 01:12:58 -0600

    updated js

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
+const inputEl = document.getElementById("city-input");
+const searchEl = document.getElementById("search-button");
+const clearEl = document.getElementById("clear-history");
+const nameEl = document.getElementById("city-name");
+const currentPicEl = document.getElementById("current-pic");
+const currentTempEl = document.getElementById("temperature");
+const currentHumidityEl = document.getElementById("humidity");
+const currentWindEl = document.getElementById("wind-speed");
+const currentUVEl = document.getElementById("UV-index");
+const historyEl = document.getElementById("history");
+let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 
+const APIKey = "ef22d2fa9f5c4aebf90ee9441d16ee47";
+
+function getWeather() {
+  var requestUrl =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    "austin" +
+    "&appid=" +
+    APIKey +
+    "&units=imperial";
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
